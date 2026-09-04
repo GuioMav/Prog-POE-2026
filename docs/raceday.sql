34ce33e4cdb9a6b37458bc708a66dce91c23dcc9
guiomav <guimavungo@gmail.com>
2026-09-04 13:03:04 +0200

    Create Users table

--- docs/raceday.sql
+++ docs/raceday.sql
@@ -1,3 +1,21 @@
 CREATE DATABASE race;
 GO
 
+CREATE TABLE Users
+(
+    usersID INT IDENTITY(1,1),
+    Email VARCHAR(50) NOT NULL,
+    userName VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    AccountType VARCHAR(20) NOT NULL,
+
+    CONSTRAINT PK_Users_usersID
+        PRIMARY KEY(usersID),
+
+    CONSTRAINT UQ_Users_Email
+        UNIQUE(Email),
+
+    CONSTRAINT UQ_Users_userName
+        UNIQUE(userName)
+);
+GO
